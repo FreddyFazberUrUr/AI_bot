3a0756f0b71c6fd342af149f8f8a85fd9f0f2550
FreddyFazberUrUr <152086433+FreddyFazberUrUr@users.noreply.github.com>
2024-02-23 12:02:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 import telebot
 import config
 import logging
-import gpt
+from gpt import GPT
+
+gpt = GPT()
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="log_file.txt",
+    filemode="w",
+    encoding='windows-1251'
 )
 
 bot = telebot.TeleBot(token=config.token)
@@ -20,7 +26,8 @@ def help_command(message):
 @bot.message_handler(commands=['about'])
 def about_command(message):
     bot.send_message(message.from_user.id, text="Рад, что ты заинтересован_а! Мое предназначение — не оставлять тебя в "
-                                                "одиночестве и всячески подбадривать!")
+                                                "одиночестве и всячески подбадривать!"
+                                                "Ссылка на гитхаб: https://github.com/FreddyFazberUrUr/AI_bot.git")
 
 
 @bot.message_handler(commands=['start', 'go'])
@@ -28,18 +35,42 @@ def start(msg):
     bot.send_message(msg.chat.id, f"Привет, {msg.from_user.first_name} ! Я - твой цифровой собеседник. Узнать обо "
                      f"мне подробнее можно командой /about\n"
                      "Введи /help для помощи в управлении ботом.\n"
-                     "Введи любой текст для того, чтобы начать разговор с нейросетью.")
+                     "<b>Введи любой текст для того, чтобы начать разговор с нейросетью.</B>", parse_mode='html')
 
 
 @bot.message_handler(commands=['debug'])
 def debug(msg):
-    bot.send_message(msg.chat.id, 'Файл с логами')
+    bot.send_document(msg.chat.id, open('log_file.txt'))
 
 
 @bot.message_handler(content_types=['text'])
 def ask_gpt(msg):
-    bot.send_message(msg.chat.id, gpt.ask_gpt(msg.text))
+    user_request = msg.text
+    request_tokens = gpt.count_tokens(user_request)
+
+    if request_tokens > gpt.MAX_TOKENS:
+        bot.send_message(msg.chat.id, 'Запрос слишком длинный. Попробуйте сделать его меньше.')
+        return
+
+    if user_request.lower() != 'продолжи':
+        gpt.clear_history()
+
+    json = gpt.make_promt(user_request)
+    logging.debug('Промт успешно сформирован.')
+
+    resp = gpt.send_request(json)
+    logging.debug('Ответ от нейросети получен.')
+
+    response = gpt.process_resp(resp)
+    logging.info('С ответом от нейросети все в порядке')
+
+    if not response[0]:
+        bot.send_message(msg.chat.id, 'Не удалось получить ответ от нейросети. Попробуйте позже.')
+    else:
+        bot.send_message(msg.chat.id, response[1])
+        logging.info('С ответом от нейросети все в порядке')
 
 
 if __name__ == '__main__':
+    logging.info("Бот запущен")
     bot.infinity_polling()
